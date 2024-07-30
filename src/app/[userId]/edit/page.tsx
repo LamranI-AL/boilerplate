c8042e2ad4644410527b31c47a7b91405cedcced
othmane LAMRANI ALAOUI <lamraniotman000@gmail.com>
2024-07-30 17:11:31 +0100

    deploy test2

--- src/app/[userId]/edit/page.tsx
+++ src/app/[userId]/edit/page.tsx
@@ -2,18 +2,6 @@ import { GetUserByID } from "@/_services/GetCurrentUser";
 import { auth } from "@/auth";
 import UpdateUserForm from "../_components/update-user";
 import { User } from "@/interfaces/Interfaces";
-import { Button } from "@/components/ui/button";
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog";
 interface Props {
   params: {
     userId: string;
@@ -34,39 +22,3 @@ async function page({ params }: Props) {
 }
 
 export default page;
-
-// import {
-//   AlertDialog,
-//   AlertDialogAction,
-//   AlertDialogCancel,
-//   AlertDialogContent,
-//   AlertDialogDescription,
-//   AlertDialogFooter,
-//   AlertDialogHeader,
-//   AlertDialogTitle,
-//   AlertDialogTrigger,
-// } from "@/components/ui/alert-dialog";
-// import { Button } from "@/components/ui/button";
-
-export function AlertDialogDemo() {
-  return (
-    <AlertDialog>
-      <AlertDialogTrigger asChild>
-        <Button variant="outline">Show Dialog</Button>
-      </AlertDialogTrigger>
-      <AlertDialogContent>
-        <AlertDialogHeader>
-          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-          <AlertDialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
-          </AlertDialogDescription>
-        </AlertDialogHeader>
-        <AlertDialogFooter>
-          <AlertDialogCancel>Cancel</AlertDialogCancel>
-          <AlertDialogAction>Continue</AlertDialogAction>
-        </AlertDialogFooter>
-      </AlertDialogContent>
-    </AlertDialog>
-  );
-}
