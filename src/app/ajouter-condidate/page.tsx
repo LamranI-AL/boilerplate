4b9ccaa3b2021df2d559f237532f2eb72037d683
othmane LAMRANI ALAOUI <lamraniotman000@gmail.com>
2024-07-31 23:35:27 +0100

    chnging passin props to addCondidat form

--- src/app/ajouter-condidate/page.tsx
+++ src/app/ajouter-condidate/page.tsx
@@ -1,13 +1,10 @@
 import AddCondidate from "@/_components/addCondidat";
-import { auth } from "@/auth";
-import { Sanction } from "@/interfaces/Interfaces";
 import React from "react";
 
 async function page() {
-  const session: Sanction | any = await auth();
   return (
     <div className="">
-      <AddCondidate session={session} />
+      <AddCondidate />
     </div>
   );
 }
